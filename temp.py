dfba3b3bc4f584e59138b686887e257ad5551079
Varunotelli <varun.kebab@gmail.com>
2018-02-01 21:47:38 +0530

    Maps API fixed

--- temp.py
+++ temp.py
@@ -38,6 +38,31 @@ def index():
         return jsonify(summary=route["summary"], duration=route["duration"], steps=route["steps"])
     return render_template('forms.html')
 
+@app.route('/query')
+def query():
+    data = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + request.args.get('origin') +'&destination=' +request.args.get('dest') +'&mode=' + request.args.get('mode').lower() +'&key=' + key)
+    resp = data.text
+    jsondata = json.loads(resp)
+    print(jsondata)
+    
+    routes = list()
+    route = dict()
+    route["steps"] = list()
+    print(len(jsondata["routes"]))
+    for r0 in jsondata["routes"]:
+        route["summary"] = r0["summary"]
+        for r1 in r0["legs"]:
+
+            route["duration"] = r1["duration"]["text"]
+            for r2 in r1["steps"]:
+                route["steps"].append(r2["html_instructions"])
+
+        # route=jsondata["routes"][0]["legs"][0]["steps"][0]["html_instructions"]
+        # duration=jsondata["routes"][0]["legs"][0]["duration"]["text"]
+        # print(route)
+            
+    return jsonify(summary=route["summary"], duration=route["duration"], steps=route["steps"])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
